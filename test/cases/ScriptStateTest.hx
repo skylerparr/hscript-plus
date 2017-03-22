afe4454332c1480b43528ce3f2e58f07de397f9f
Dlean Jeans <dleanjeans@gmail.com>
2017-03-22 12:08:02 +0700

    ScriptStateTest: Add testReadmeUsageExample

--- test/cases/ScriptStateTest.hx
+++ test/cases/ScriptStateTest.hx
@@ -12,6 +12,36 @@ class ScriptStateTest {
         state = new ScriptState();
     }
 
+    public function testReadmeUsageExample() {
+        var script = "
+        class Object {
+            // main() is called automatically when script is executed
+            public static function main() {
+                var object = new Object(10, 10);
+                object.name = NAME;
+                Assert.equals('Ball', object.name);
+                Assert.equals(10, object.x);
+                Assert.equals(10, object.y);
+            }
+            
+            public var x:Float = 0;
+            public var y:Float = 0;
+
+            public function new(x:Float, y:Float) {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        ";
+        state.set("Assert", Assert);
+        state.set("NAME", "Ball"); // set a global variable
+        state.executeString(script); // executes a String
+
+        // get a global variable
+        var Object = state.get("Object"); // get a global variable
+        Object.main();
+    }
+
     public function testClassCreated() {
         var script = '
         class Object {
